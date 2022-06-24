f874b9cc9bcf96296b7dde8152a72da8ab22eabf
digitalvysh <vyshnavibandla@gmail.com>
2022-06-24 22:28:24 +0530

    done

--- backend/routes/router.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const getPosts = require("../controllers/getPosts");
-const addPosts = require("../controllers/addPosts");
-const upload = require("../utils/multer");
-
-router.route("/posts").get(getPosts).post(upload.single("image"), addPosts);
-
-module.exports = router;
